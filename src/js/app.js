e406eb4ca68de22fdefad46adbfc34068b84ce2a
Steven Mulder <steven.mulder@gmail.com>
2015-08-21 11:43:49 +0200

    Add basic 'join' button without handler
    This React stuff is hard to grasp.

--- src/js/app.js
+++ src/js/app.js
@@ -50,88 +50,48 @@ var MainViewController = React.createClass({
 		return (
 			<Container>
 				<UI.NavigationBar name="main" />
-				<ViewManager name="main" defaultView="tabs">
-					<View name="tabs" component={TabViewController} />
+				<ViewManager name="main" defaultView="join">
+					<View name="join" component={JoinViewController} />
 				</ViewManager>
 			</Container>
 		);
 	}
 });
 
-// Tab Controller
+// Join View Controller
 // ------------------------------
 
-var lastSelectedTab = 'lists'
-var TabViewController = React.createClass({
-	getInitialState () {
-		return {
-			selectedTab: lastSelectedTab
-		};
+var JoinViewController = React.createClass({
+	statics: {
+		navigationBar: 'main',
+		getNavigation () {
+			return {
+				title: 'Ratings'
+			}
+		}
 	},
 
-	onViewChange (nextView) {
-		lastSelectedTab = nextView
-
-		this.setState({
-			selectedTab: nextView
+	createGuid () {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
 		});
 	},
-
-	selectTab (value) {
-		let viewProps;
-
-		this.refs.vm.transitionTo(value, {
-			transition: 'instant',
-			viewProps: viewProps
-		});
-
-		this.setState({
-			selectedTab: value
-		})
+	join () {
+		console.log('joining...', this.createGuid());
 	},
-
 	render () {
-		let selectedTab = this.state.selectedTab
-		let selectedTabSpan = selectedTab
-
-		if (selectedTab === 'lists' || selectedTab === 'list-simple' || selectedTab === 'list-complex' || selectedTab === 'list-details') {
-			selectedTabSpan = 'lists';
-		}
-
-		if (selectedTab === 'transitions' || selectedTab === 'transitions-target') {
-			selectedTabSpan = 'transitions';
-		}
-
 		return (
 			<Container>
-				<ViewManager ref="vm" name="tabs" defaultView={selectedTab} onViewChange={this.onViewChange}>
-					<View name="lists" component={require('./views/lists')} />
-					<View name="list-simple" component={require('./views/list-simple')} />
-					<View name="list-complex" component={require('./views/list-complex')} />
-					<View name="list-details" component={require('./views/list-details')} />
-					<View name="form" component={require('./views/form')} />
-					<View name="controls" component={require('./views/controls')} />
-					<View name="transitions" component={require('./views/transitions')} />
-					<View name="transitions-target" component={require('./views/transitions-target')} />
-				</ViewManager>
-				<UI.Tabs.Navigator>
-					<UI.Tabs.Tab onTap={this.selectTab.bind(this, 'lists')} selected={selectedTabSpan === 'lists'}>
-						<span className="Tabs-Icon Tabs-Icon--lists" />
-						<UI.Tabs.Label>Lists</UI.Tabs.Label>
-					</UI.Tabs.Tab>
-					<UI.Tabs.Tab onTap={this.selectTab.bind(this, 'form')} selected={selectedTabSpan === 'form'}>
-						<span className="Tabs-Icon Tabs-Icon--forms" />
-						<UI.Tabs.Label>Forms</UI.Tabs.Label>
-					</UI.Tabs.Tab>
-					<UI.Tabs.Tab onTap={this.selectTab.bind(this, 'controls')} selected={selectedTabSpan === 'controls'}>
-						<span className="Tabs-Icon Tabs-Icon--controls" />
-						<UI.Tabs.Label>Controls</UI.Tabs.Label>
-					</UI.Tabs.Tab>
-					<UI.Tabs.Tab onTap={this.selectTab.bind(this, 'transitions')} selected={selectedTabSpan === 'transitions'}>
-						<span className="Tabs-Icon Tabs-Icon--transitions" />
-						<UI.Tabs.Label>Transitions</UI.Tabs.Label>
-					</UI.Tabs.Tab>
-				</UI.Tabs.Navigator>
+				<UI.Group>
+					<UI.GroupHeader>Greetings, stranger</UI.GroupHeader>
+					<UI.GroupBody>
+						<UI.GroupInner>
+							<p>Welcome to the Ratings app. Join a session to start rating the talks.</p>
+						</UI.GroupInner>
+						<UI.Button type="primary" onTap={this.join}>JOIN SESSION</UI.Button>
+					</UI.GroupBody>
+				</UI.Group>
 			</Container>
 		);
 	}
